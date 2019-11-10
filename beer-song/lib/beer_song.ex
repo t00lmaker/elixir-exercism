2c8d3fdecfb004ad55b93661629b357bad6a670e
luan <luan@infoway-pi.com.br>
2019-11-10 15:45:37 -0300

    solution problem

---
+++ beer-song/lib/beer_song.ex
@@ -0,0 +1,34 @@
+defmodule BeerSong do
+  import String 
+
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t()
+  def verse(number) do
+    """
+    #{ bottle(number) |> capitalize  } of beer on the wall, #{ bottle(number) } of beer.
+    #{ action(number) }, #{ bottle(number - 1) } of beer on the wall.
+    """
+  end  
+  
+  @doc """
+  Get the entire beer song for a given range of numbers of bottles.
+  """
+  @spec lyrics(Range.t()) :: String.t()
+  def lyrics(range) do
+    Enum.reduce(range, "", &(&2 <> verse(&1) <> "\n")) |> slice(0..-2)
+  end
+  def lyrics(), do: lyrics(99..0)
+  
+  defp bottle(number) when number < 0, do: bottle(99)
+  defp bottle(0), do: "no more bottles"
+  defp bottle(1), do: "1 bottle"
+  defp bottle(number), do: "#{number} bottles" 
+
+  defp action(0), do: "Go to the store and buy some more"
+  defp action(1), do: "Take it down and pass it around"
+  defp action(_), do: "Take one down and pass it around"
+
+end
+
